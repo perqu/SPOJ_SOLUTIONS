51f63fc705427bd88adb2aed7fc1a68d5cf78d51
Paweł Perenc <pawelkperenc@gmail.com>
2021-03-18 10:56:06 +0100

    comments
    added comments

--- SPOJ [439] Not so fast multiplication.py
+++ SPOJ [439] Not so fast multiplication.py
@@ -1,4 +1,13 @@
 def multiplication(a,b):
+    """Returns multiplication of 2 numbers
+    
+    Args:        
+        a: first number of type int
+        b: second number of type int
+
+    Returns:        
+        A multiplication for that two numbers of type int
+    """
     return a*b
 
 n = int(input())
--- SPOJ [522] Przedszkolanka (NWW - least common multiple).py
+++ SPOJ [522] Przedszkolanka (NWW - least common multiple).py
@@ -1,9 +1,27 @@
 def GCD(a, b): #NWD
+    """Counts GCD of two numbers
+    
+    Args:        
+        a: first number of type int
+        b: second number of type int
+
+    Returns:        
+        A GCD for that two numbers of type int
+    """
     if b > 0:
         return GCD(b, a%b)
     return a
 
 def LCM(a,b): #NWW
+    """Counts LCM using base of GCD
+    
+    Args:        
+        a: first number of type int
+        b: second number of type int
+
+    Returns:        
+        A LCM for that two numbers of type int
+    """
     return (a*b)//(GCD(a,b))
 
 n = int(input())
